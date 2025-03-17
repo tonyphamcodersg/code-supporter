cd68e24b60aa1d0d25a9d88edea914455485acde
tamphu.pn <tamphu.pn>
2025-03-17 16:20:58 +0700

    #13: add test

--- CodeSupporter/CleanArchitecture/tests/Cassie.Domain.Tests/Common/EntityDefinition/AuditableWithIdEntityTests.cs
+++ CodeSupporter/CleanArchitecture/tests/Cassie.Domain.Tests/Common/EntityDefinition/AuditableWithIdEntityTests.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Cassie.Domain.Common.EntityDefinition;
+using System;
+using Xunit;
 
 namespace Cassie.Domain.Tests.Common.EntityDefinition
 {
-    internal class AuditableWithIdEntityTests
+    public class MockTestAuditableWithIdEntity : AuditableWithIdEntity<Guid, Guid> { }
+
+    public class AuditableWithIdEntityTests
     {
+        [Fact]
+        public void Should_Set_Id_Correctly()
+        {
+            //Arrange
+            var entity = new MockTestAuditableWithIdEntity();
+            var createdBy = Guid.NewGuid();
+            var created = DateTimeOffset.UtcNow;
+            var id = Guid.NewGuid();
+
+            //Act
+            entity.CreatedBy = createdBy;
+            entity.Created = created;
+            entity.Id = id;
+
+            //Assert
+            Assert.Equal(entity.Id, id);
+        }
     }
 }
--- CodeSupporter/CleanArchitecture/tests/Cassie.Domain.Tests/Common/EntityDefinition/EntityTests.cs
+++ CodeSupporter/CleanArchitecture/tests/Cassie.Domain.Tests/Common/EntityDefinition/EntityTests.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Cassie.Domain.Common.EntityDefinition;
+using System;
+using Xunit;
 
 namespace Cassie.Domain.Tests.Common.EntityDefinition
 {
-    internal class EntityTests
+    public class MockTestEntity : Entity<Guid> { }
+
+    public class EntityTests
     {
+        [Fact]
+        public void Should_Set_Id_Correctly()
+        {
+            //Arrange
+            var entity = new MockTestEntity();
+            var id = Guid.NewGuid();
+
+            //Act
+            entity.Id = id;
+
+            //Assert
+            Assert.Equal(entity.Id, id);
+        }
     }
 }
